8a920f572388c1b7154cc9bfba5cc0bfdcfd3264
Siroj <ISGamesDeveloper@gmail.com>
2024-10-24 17:39:14 +0300

    update

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DemoBuild-FamilyTree-1.0";
+const contentToCache = [
+    "Build/FamilyTreeBuild.loader.js",
+    "Build/FamilyTreeBuild.framework.js",
+    "Build/FamilyTreeBuild.data",
+    "Build/FamilyTreeBuild.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
